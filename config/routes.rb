c1bf50033293441e106b28e2403ba9afe4c77a41
Craig Smith <nctiggy@gmail.com>
2012-05-11 19:35:40 -0700

    Added static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Is312Site::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/family"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
